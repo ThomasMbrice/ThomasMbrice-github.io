d4323a2bea6181d1f2627d12a2cc8c0146d3dbcf
recursivebanana <officialtmbrice@gmail.com>
2024-08-09 20:28:00 -0400

    Initial commit with existing code

---
+++ Desktop/portfolliotest/main.js
@@ -0,0 +1,120 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';   
+
+
+// Function to redirect based on screen size
+function checkScreenSize() {
+  // Define the breakpoint width (e.g., 1024px for lower than a laptop)
+  const breakpointWidth = 1024;
+
+  // Check the current screen width
+  if (window.innerWidth < breakpointWidth) {
+      // Redirect to a different page if the screen width is below the breakpoint
+      window.location.href = "page.html"; // Replace with your URL
+  }
+}
+
+// Execute the function on page load
+window.addEventListener('load', checkScreenSize);
+
+// Optional: Re-check on window resize
+window.addEventListener('resize', checkScreenSize);
+
+
+
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.animating   
+ = true;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);   
+
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(-4, 5, -11);
+camera.lookAt(0,0,0);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+groundGeometry.rotateX(-Math.PI / 2);
+const groundMaterial = new THREE.MeshStandardMaterial({
+  color: 0x555555,
+  side: THREE.DoubleSide
+});
+const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+groundMesh.castShadow = false;
+groundMesh.receiveShadow = true;
+scene.add(groundMesh);
+
+const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
+spotLight.position.set(0, 25, 0);
+//spotLight.castShadow = true;
+//spotLight.shadow.bias = -0.0001;
+scene.add(spotLight);
+
+const loader = new GLTFLoader().setPath('public/scifi_chair_concept/');
+loader.load('scene.gltf', (gltf) => {
+  console.log('loading model');
+  const mesh = gltf.scene;
+
+  mesh.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  mesh.position.set(0, 2.5, 50);
+  scene.add(mesh);
+
+  document.getElementById('progress-container').style.display = 'none';
+}, (xhr) => {
+  console.log(`loading ${xhr.loaded / xhr.total * 100}%`);
+}, (error) => {
+  console.error(error);
+});
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+const videoElement = document.getElementById('myVideo');
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        // Redirect to the video page
+        window.location.href = 'video_page.html';
+    }
+});
+
+
+animate();
---
+++ Desktop/portfolliotest/script.js
@@ -0,0 +1,58 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelectorAll('header nav a [href*=' + id +" ]")
+                .classList.add('active')
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+function checkScreenSize() {
+    const breakpointWidth = 1024;
+    const popup = document.getElementById('popup');
+
+    if (window.innerWidth < breakpointWidth) {
+        popup.classList.add('show');
+
+        setTimeout(() => {
+            popup.classList.remove('show');
+        }, 10000); // Hide after 10 seconds
+    }
+}
+
+// Execute the function on page load
+window.addEventListener('load', checkScreenSize);
+
+// Optional: Re-check on window resize
+window.addEventListener('resize', checkScreenSize);
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('downloadBtn').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default anchor click behavior
+
+        var link = document.createElement('a');
+        link.href = 'public/resume/Engineering-Technical-Resume.pdf.pdf'; // Path to your resume
+        link.download = 'ThomasMbriceResume.pdf'; // Name of the file to be downloaded
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+});
